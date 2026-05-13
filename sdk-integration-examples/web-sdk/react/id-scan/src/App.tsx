854139e852f7a0bb59eda490d444440fa1043d4a
Nicklas Warming Jacobsen <nicklas.jacobsen@lexisnexisrisk.com>
2026-05-13 13:43:30 +0200

    Port react id-scan

--- sdk-integration-examples/web-sdk/react/id-scan/src/App.tsx
+++ sdk-integration-examples/web-sdk/react/id-scan/src/App.tsx
@@ -1,17 +1,16 @@
+const sessionUrl = import.meta.env.VITE_SDK_SESSION_URL;
+const sessionToken = import.meta.env.VITE_SDK_SESSION_TOKEN;
+
 import { useEffect, useState } from "react";
 import idverselogo from "/logo.svg";
 import loadingSvg from "./assets/loading.svg";
 import "./App.css";
 
-const sessionUrl = import.meta.env.VITE_SDK_SESSION_URL;
-const sessionToken = import.meta.env.VITE_SDK_SESSION_TOKEN;
-const buildId = import.meta.env.VITE_SDK_SESSION_BUILD_ID;
-
 import {
   IDScanRecognizerResult,
-  IdverseSdkUiCustomEvent,
+  IdvSdkWebCustomEvent,
   SdkType
-} from "@idverse/idverse-sdk-ui";
+} from "@idverse/idv-sdk-web";
 
 import { Details } from "./components/Details/Details";
 
@@ -21,7 +20,7 @@ function App() {
   const [error, setError] = useState<string>();
   const [_scanBothSides, setScanBothSides] = useState(false);
   const [resultData, setResultData] = useState<IDScanRecognizerResult>();
-  const [idverseSDK, setIdverseSDK] = useState<HTMLIdverseSdkUiElement | null>(
+  const [idverseSDK, setIdverseSDK] = useState<HTMLIdvSdkWebElement | null>(
     null
   );
 
@@ -31,27 +30,27 @@ function App() {
     console.log("Successfully loaded");
   };
 
-  const onScanSuccess = (ev: IdverseSdkUiCustomEvent<any>) => {
+  const onScanSuccess = (ev: IdvSdkWebCustomEvent<any>) => {
     console.log(ev.detail);
     setResultData(ev.detail.result.details.extractedInfo.viz);
   };
 
-  const onScanFail = (ev: IdverseSdkUiCustomEvent<any>) => {
+  const onScanFail = (ev: IdvSdkWebCustomEvent<any>) => {
     console.log("failed to scan.", ev);
     setError(ev.detail.toString());
   };
 
-  const onError = (e: IdverseSdkUiCustomEvent<any>) => {
+  const onError = (e: IdvSdkWebCustomEvent<any>) => {
     setLoading(false);
     console.error("SDKError", e.detail);
     setError(e.detail.message.toString());
   };
 
-  const onFirstScan = (e: IdverseSdkUiCustomEvent<any>) => {
+  const onFirstScan = (e: IdvSdkWebCustomEvent<any>) => {
     console.log("first scan", e);
   };
 
-  const onAuthenticationSuccess = (e: IdverseSdkUiCustomEvent<any>) => {
+  const onAuthenticationSuccess = (e: IdvSdkWebCustomEvent<any>) => {
     console.log("authentication success", e);
     setLoading(false);
     setReady(true);
@@ -63,10 +62,10 @@ function App() {
 
   useEffect(() => {
     const sdk = document.querySelector(
-      "idverse-sdk-ui"
-    ) as HTMLIdverseSdkUiElement;
+      "idv-sdk-web"
+    ) as HTMLIdvSdkWebElement;
     if (!sdk) {
-      throw "idverse-sdk-ui tag does not exist";
+      throw "idv-sdk-web tag does not exist";
     }
     sdk.recognizers = [SdkType.IDScan];
     sdk.enableDFA = true;
@@ -139,11 +138,10 @@ function App() {
         />
       )}
 
-      {/* Initialize endpoint is called as soon as <idverse-sdk-ui/> is in the DOM */}
-      <idverse-sdk-ui
+      {/* Initialize endpoint is called as soon as <idv-sdk-web/> is in the DOM */}
+      <idv-sdk-web
         session-url={sessionUrl}
         session-token={sessionToken}
-        session-build-id={buildId}
         // Use `enable-dfa` prop to choose whether or not DFA engine is enabled, if value is static and will not change
         // If for some reason value is dynamic (needs to change) use `sdk.setEnableDFA()`
         enable-dfa={true}
